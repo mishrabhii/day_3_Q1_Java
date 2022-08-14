99df1ecf026646751d44f05a876068a772cdfa03
mishrabhii <abhi.mishra509@gmail.com>
2022-08-15 01:03:13 +0530

    initial commit

---
+++ src/DisplayPackage/DisplayWelcomeToEmployee.java
@@ -0,0 +1,8 @@
+package DisplayPackage;
+
+public class DisplayWelcomeToEmployee {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+
+}
